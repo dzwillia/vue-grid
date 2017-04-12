4fb7914bb7a08f2d0ed48303bde6303158f9550f
David Z. Williams <dave@flex.io>
2017-04-12 00:50:26 -0400

    Split out simple sample data into separate file.

---
+++ src/data/simple.js
@@ -0,0 +1,82 @@
+export default {
+  total_count: 10,
+  columns: [
+    { name: 'id' },
+    { name: 'first_name' },
+    { name: 'last_name' },
+    { name: 'email' },
+    { name: 'gender' },
+    { name: 'ip_address' }
+  ],
+  rows: [{
+    id: 1,
+    first_name: 'Phyllis',
+    last_name: 'Johnston',
+    email: 'pjohnston0@drupal.org',
+    gender: 'Female',
+    ip_address: '210.13.57.74'
+  },{
+    id: 2,
+    first_name: 'Billy',
+    last_name: 'Olson',
+    email: 'bolson1@altervista.org',
+    gender: 'Male',
+    ip_address: '51.200.83.232'
+  },{
+    id: 3,
+    first_name: 'Peter',
+    last_name: 'Porter',
+    email: 'pporter2@ed.gov',
+    gender: 'Male',
+    ip_address: '214.130.223.48'
+  },{
+    id: 4,
+    first_name: 'Alan',
+    last_name: 'Chapman',
+    email: 'achapman3@addtoany.com',
+    gender: 'Male',
+    ip_address: '105.113.204.220'
+  },{
+    id: 5,
+    first_name: 'Amanda',
+    last_name: 'Murray',
+    email: 'amurray4@fda.gov',
+    gender: 'Female',
+    ip_address: '229.239.160.173'
+  },{
+    id: 6,
+    first_name: 'Heather',
+    last_name: 'Wood',
+    email: 'hwood5@tmall.com',
+    gender: 'Female',
+    ip_address: '39.18.172.229'
+  },{
+    id: 7,
+    first_name: 'Peter',
+    last_name: 'Hunter',
+    email: 'phunter6@is.gd',
+    gender: 'Male',
+    ip_address: '132.201.130.155'
+  },{
+    id: 8,
+    first_name: 'Sean',
+    last_name: 'Hamilton',
+    email: 'shamilton7@fotki.com',
+    gender: 'Male',
+    ip_address: '128.145.63.156'
+  },{
+    id: 9,
+    first_name: 'Juan',
+    last_name: 'Castillo',
+    email: 'jcastillo8@pcworld.com',
+    gender: 'Male',
+    ip_address: '207.133.114.80'
+  },{
+    id: 10,
+    first_name: 'Deborah',
+    last_name: 'Harper',
+    email: 'dharper9@cargocollective.com',
+    gender: 'Female',
+    ip_address: '164.129.103.105'
+  }]
+}
